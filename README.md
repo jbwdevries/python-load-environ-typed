4280e6ae9b98a6a8069203f57a4580924094ecd6
Johan B.W. de Vries <info@jbwdevries.nl>
2023-07-09 07:11:34 +0200

    Various utility improvements
    Support:
    - bool using true/false
    - datetime's date, time and datetime using fromisoformat
    - kw_only for dataclass
    - Optional
    
    Also various documentation

--- README.md
+++ README.md
@@ -19,6 +19,118 @@ class MyEnviron:
 environ = load(MyEnviron)
 ```
 
+## FAQ
+
+### What types can I use? What about custom types?
+
+By default, any type that takes a single string in it's constructor can be used
+as type. Think of `int`, `str`, `float`, `pathlib.Path`, etc.
+
+We've added default loaders for the types below:
+
+- `bool` - "true", "FalSe"
+- `datetime.date` - Using `datetime.date.fromisoformat`
+- `datetime.time` - Using `datetime.time.fromisoformat`
+- `datetime.datetime` - Using `datetime.datetime.fromisoformat`
+
+### Can values be optional?
+
+Certainly:
+
+```python
+@dataclass
+class MyEnviron:
+	DB_HOST: Optional[str]
+```
+
+However:
+
+- empty string and "none" count as `None`
+- defaults take precedence over optionality
+- this does not work for dataclass properties
+
+### Can I use Unions?
+
+You can, but you need to use a default loader, as you need some
+way to distinguish between the types, and there is no general
+way to do so, at least not without enforcing our way of working on you.
+
+### What if my type cannot take a string in its constructor?
+
+You can pass so-called loader functions. These take in a string, and are
+expected to return a value of the given type, or raise a `ValueError` when
+the given string is not valid for the given type. This is also the
+mechanism that we use to support standard Python types such `datetime.date`,
+which is shown below.
+
+```python
+@dataclass
+class MyEnviron:
+    ISO_DATE: datetime.date
+
+environ = sut.load(MyEnviron, environ={
+    'ISO_DATE': '2021-01-01',
+}, loaders={
+    'ISO_DATE': datetime.date.fromisoformat,
+})
+
+self.assertEqual(datetime.date(2021, 1, 1), environ.ISO_DATE)
+```
+
+NOTE: date has a default loader, so you don't need to do this for `date`.
+
+### How do I work with default values?
+
+If you want default values, it's probably best to have use a dataclass
+with `kw_only=True`, as otherwise you have to order your variables based
+on whether there's a default or not.
+
+```python
+@dataclass(kw_only=True)
+class MyEnviron:
+	DB_HOST: str
+	DB_PORT: int = 3306
+```
+
+NOTE: `kw_only` requires Python3.10 or higher
+
+For most types, you can simply set the default value as you're used to
+with dataclasses. However, you may not want to instantiate an (expensive)
+property as default. In those cases, you can pass defaults along using
+the `defaults` argument.
+
+```python
+@dataclass(kw_only=True)
+class MyEnviron:
+	VAR: SomeExpensiveClass
+
+environ = load(MyEnviron, defaults={
+	'VAR': '#!serialized.data!#',
+})
+```
+
+### Load returns an instance. What if I want a global?
+
+Since `environ` is available at startup, and doesn't change, it's perfectly
+valid to just instantiate a global variable. You should probably use a
+frozen dataclass for this.
+
+```python
+@dataclass(frozen=True)
+class MyEnviron:
+	DB_HOST: str
+	DB_PORT: int
+
+ENVIRON = load(MyEnviron)
+```
+
+### What if there's an issue with the default loaders?
+
+First, the loaders you pass will be taken before using the default loaders.
+
+Second, if you have more structural issues with the default loaders, simply
+pass `use_default_loaders=False`.
+
 ## Contributing
 
 Check out this repo, and then use the following steps to test it:
--- load_environ_typed/__init__.py
+++ load_environ_typed/__init__.py
@@ -1,6 +1,10 @@
-from typing import Any, Callable, List, Mapping, Optional, Type, TypeVar
+from typing import (
+    Any, Callable, Dict, List, Mapping, Optional,
+    Tuple, Type, TypeVar, Union
+)
 
 import dataclasses
+import datetime
 import os
 
 T = TypeVar('T')
@@ -10,12 +14,31 @@ class LoadEnvironmentException(BaseException):
     pass
 
 
+def load_bool(raw: str) -> bool:
+    if 'true' == raw.lower():
+        return True
+
+    if 'false' == raw.lower():
+        return False
+
+    raise ValueError(f'"{raw}" cannot be parsed as boolean')
+
+
+DEFAULT_LOADERS = {
+    bool: load_bool,
+    datetime.date: datetime.date.fromisoformat,
+    datetime.time: datetime.time.fromisoformat,
+    datetime.datetime: datetime.datetime.fromisoformat,
+}
+
+
 def load(
     type_: Type[T],
     *,
     environ: Optional[Mapping[str, str]] = None,
     defaults: Optional[Mapping[str, str]] = None,
     loaders: Optional[Mapping[str, Callable[[str], Any]]] = None,
+    use_default_loaders: bool = True,
 ) -> T:
     """
     Loads the data from environ into the given class
@@ -46,13 +69,24 @@ def load(
                 f'{type_!r} is not a dataclasses.dataclass'
                 ' and not a typing.NamedTuple')
 
-    args: List[Any] = []
+    kwargs: Dict[str, Any] = {}
 
     errors: List[str] = []
 
     for field_name in field_name_list:
         field_type = type_.__annotations__[field_name]
-        field_loader = loaders.get(field_name, field_type)
+        field_type, is_optional_type = check_optional(field_type)
+
+        try:
+            field_loader = loaders[field_name]
+        except KeyError:
+            try:
+                if not use_default_loaders:
+                    raise KeyError
+
+                field_loader = DEFAULT_LOADERS[field_type]
+            except KeyError:
+                field_loader = field_type
 
         try:
             field_value_str = environ[field_name]
@@ -62,13 +96,20 @@ def load(
             except KeyError:
                 try:
                     field_value = typed_defaults[field_name]
-                    args.append(field_value)
+                    kwargs[field_name] = field_value
                 except KeyError:
-                    errors.append(
-                        'No value in environ for required property'
-                        f' {field_name} of type '
-                        f'{field_type.__module__}.{field_type.__name__}')
+                    if is_optional_type:
+                        kwargs[field_name] = None
+                    else:
+                        errors.append(
+                            'No value in environ for required property'
+                            f' {field_name} of type '
+                            f'{field_type.__module__}.{field_type.__name__}')
                 continue
+
+        if is_optional_type and is_optional_value(field_value_str):
+            kwargs[field_name] = None
+            continue
 
         field_loader = getattr(
             field_loader, 'load_environ_typed', field_loader)
@@ -83,9 +124,41 @@ def load(
             )
             continue
 
-        args.append(field_value)
+        kwargs[field_name] = field_value
 
     if errors:
         raise LoadEnvironmentException(errors)
 
-    return type_(*args)
+    return type_(**kwargs)
+
+
+def check_optional(type_: Type[Any]) -> Tuple[Type[Any], bool]:
+    """
+    Checks whether the given type is an Optional variant
+
+    This has some wonkyness regarding which Python version we're on
+    """
+    if getattr(type_, '__origin__', None) is Optional:
+        raise NotImplementedError('TODO')
+
+    if getattr(type_, '__origin__', None) is Union:
+        args = tuple(
+            x
+            for x in type_.__args__
+            if x is not None and x != type(None)
+        )
+
+        if len(args) == len(type_.__args__):
+            return type_, False
+
+        if len(args) == 1:
+            return args[0], True
+
+        # Union types need a custom loader anyhow
+        return type_, False
+
+    return type_, False
+
+
+def is_optional_value(raw: str) -> bool:
+    return raw == '' or raw.lower() == 'none'
